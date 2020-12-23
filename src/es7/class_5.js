8e560858a2d1e538f7079362edf663442577b30a
kevinmora47 <kevinmora47@gmail.com>
2020-12-23 17:04:23 -0600

    Number elevations, includes-23-12-2020

---
+++ src/es7/class_5.js
@@ -0,0 +1,15 @@
+let numbers = [1,2,3,4,5,6,7,8,9,0];
+
+if (numbers.includes(-1)) {
+    console.log('Si existe.');
+}else{
+    console.log('No esta :v');
+}
+
+// Elevate.
+
+let base = 4;
+let exponent = 3;
+let result = base ** 3;
+
+console.log(result);
