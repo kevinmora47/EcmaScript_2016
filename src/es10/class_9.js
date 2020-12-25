d9f6d44231b9b601d83365ed33c450ea032dac42
kevinmora47 <kevinmora47@gmail.com>
2020-12-25 15:07:43 -0600

    Ecmascript 10-25-12-2020

---
+++ src/es10/class_9.js
@@ -0,0 +1,11 @@
+let array = [1,2,3, [1,2,3, [1,2,3]]];
+
+console.log(array.flat(3));
+
+let array = [1,2,3,4,5];
+console.log(array.flatMap(value => [value, value *2]));
+
+
+let hello = '     hi';
+console.log(hello);
+console.log(hello.trim());
